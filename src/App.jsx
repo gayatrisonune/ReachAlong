ed49040399c7108ccf2c1d4b401ecde339b085df
Anushka Bisen <bisenanushka@gmail.com>
2023-04-08 14:23:11 +0530

    navbar_sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+// import Home from "./components/Home";
+
+import "./App.css";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
