94cc33258137afc6ee974d08408758d0aa3fdd22
gayatrisonune <gayatriashoksonune@gmail.com>
2023-04-08 15:47:08 +0530

    conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 // import { Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
+
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 export default function App() {
   return (
-    <div>
-      <Navbar />
+    <div style={{ height: "100vh", backgroundImage: 'url("https://unbounce.com/photos/Gradient-Background.png")', backgroundRepeat: "no-repeat", width: "100vw", backgroundSize: "cover", }}>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
