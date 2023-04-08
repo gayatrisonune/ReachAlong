a82d5568ce983cc1db5ff7cd057f75aa371d21c3
gayatrisonune <gayatriashoksonune@gmail.com>
2023-04-08 22:21:54 +0530

    changed js to jsx

--- src/components/VideoSidebar.jsx
+++ src/components/VideoSidebar.jsx
@@ -4,28 +4,28 @@ import FavoriteIcons from "@material-ui/icons/Message";
 import FavoriteIcons from "@material-ui/icons/Share";
 import FavoriteIcons from "@material-ui/icons/FavoriteBorder";
 import "./VideoSidebar.css"
-function VideoSidebar({likes,share,msg}) {
-    const [liked,setliked]=setliked(true);
+function VideoSidebar({ likes, share, msg }) {
+  const [liked, setliked] = setliked(true);
   return (
-    
+
     <div className='videoSidebar'>
       <div className="videoSidebar_button">
         {liked ? (
-            <FavoriteIcons  fontSize="large" onClick={(e)=>setliked(false)}/>
-        ):(<FavoriteBorderIcon
-            fontSize="large"
-            onClick={(e)=>setliked(true)}/>)
+          <FavoriteIcons fontSize="large" onClick={(e) => setliked(false)} />
+        ) : (<FavoriteBorderIcon
+          fontSize="large"
+          onClick={(e) => setliked(true)} />)
         }
-        
-        <p>{liked ? likes+1 :likes}</p>
+
+        <p>{liked ? likes + 1 : likes}</p>
       </div>
       <div className="videoSidebar_button">
-        <MessageIcon  fontSize="large"/>
+        <MessageIcon fontSize="large" />
         <p>msg</p>
       </div>
       <div className="videoSidebar_button">
-      <ShareIcon  fontSize="large"/>
-      <p>share</p>
+        <ShareIcon fontSize="large" />
+        <p>share</p>
       </div>
     </div>
   )
