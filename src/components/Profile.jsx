23f0f3fd1ae9d6ed130dde45250293edfc31c9d1
gayatrisonune <gayatriashoksonune@gmail.com>
2023-04-09 12:14:37 +0530

    Profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Navigate } from 'react-router'
 import EditProfile from './EditProfile'
 import { useNavigate } from 'react-router-dom'
+import Navbar from './Navbar';
+import "./Profile.css";
 
 export default function Profile() {
     const Navigate = useNavigate();
@@ -12,10 +14,38 @@ export default function Profile() {
 
     return (
         <div>
-            <button
-                onClick={(event) =>
-                    ClickMe(event)}>Edit Profile
-            </button>
+            <>
+                <Navbar />
+                <button
+                    onClick={(event) =>
+                        ClickMe(event)}>Edit Profile
+                </button>
+                <div className="profile">
+                    <div className="profileRight">
+                        <div className="profileRightTop">
+                            <div className="profileCover">
+                                <img
+                                    className="profileCoverImg"
+                                    src="assets/post/3.jpeg"
+                                    alt=""
+                                />
+                                <img
+                                    className="profileUserImg"
+                                    src="assets/person/7.jpeg"
+                                    alt=""
+                                />
+                            </div>
+                            <div className="profileInfo">
+                                <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                                <span className="profileInfoDesc">Hello my friends!</span>
+                            </div>
+                        </div>
+                        {/* <div className="profileRightBottom">
+            <Rightbar profile/>
+          </div> */}
+                    </div>
+                </div>
+            </>
         </div>
     )
 }
