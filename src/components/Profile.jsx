106c129a3666ff77d0c2e9df43e889676d1d137e
gayatrisonune <gayatriashoksonune@gmail.com>
2023-04-09 14:27:40 +0530

    profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,6 +5,23 @@ import { useNavigate } from 'react-router-dom'
 import Navbar from './Navbar';
 import "./Profile.css";
 
+import Button from '@mui/material/Button';
+import EditIcon from '@mui/icons-material/Edit';
+
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+
+
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import TheatersIcon from '@mui/icons-material/Theaters';
+
 export default function Profile() {
     const Navigate = useNavigate();
     const ClickMe = (event) => {
@@ -13,39 +30,64 @@ export default function Profile() {
     }
 
     return (
-        <div>
-            <>
+        <div className='bg'>
                 <Navbar />
-                <button
+                <Button variant="outlined" color="primary"
                     onClick={(event) =>
-                        ClickMe(event)}>Edit Profile
-                </button>
-                <div className="profile">
-                    <div className="profileRight">
-                        <div className="profileRightTop">
-                            <div className="profileCover">
-                                <img
-                                    className="profileCoverImg"
-                                    src="assets/post/3.jpeg"
-                                    alt=""
-                                />
-                                <img
-                                    className="profileUserImg"
-                                    src="assets/person/7.jpeg"
-                                    alt=""
-                                />
-                            </div>
-                            <div className="profileInfo">
-                                <h4 className="profileInfoName">Safak Kocaoglu</h4>
-                                <span className="profileInfoDesc">Hello my friends!</span>
-                            </div>
+                        ClickMe(event)}><EditIcon />
+                </Button>
+
+                <div className="profileCover">
+                            <img className='profileCoverImg'
+                            src="https://images.unsplash.com/photo-1554177255-61502b352de3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="profile" />
+                        
+                        <img className='profileUserImg'
+                        src="https://images.unsplash.com/photo-1509839862600-309617c3201e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="profile" />
                         </div>
-                        {/* <div className="profileRightBottom">
-            <Rightbar profile/>
-          </div> */}
-                    </div>
+                        <div className="profileinfo">
+                            <h1>Marla Seron</h1>
                 </div>
-            </>
-        </div>
+                {/* <div className='info'>
+                        <div className='objects'>
+                            <p>Reels</p>
+                        </div>
+                        <div className='objects'>
+                            <p>Subscription</p>
+                        </div>
+                        <div className='objects'>
+                            About
+                        </div> */}
+
+                        <div className='info'>
+                        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <ImageIcon />
+                            </Avatar>
+                            </ListItemAvatar >
+                            <ListItemText 
+                            primary="Reels" secondary="123" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <WorkIcon />
+                            </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Subscriber" secondary="2014" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <BeachAccessIcon />
+                            </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="About" secondary="myhappylife" />
+                        </ListItem>
+                        </List>
+                        </div>
+                    </div>
+        
     )
 }
